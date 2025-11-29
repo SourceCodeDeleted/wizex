a7704c78532ec3a724a96f6c484b3ca7475f2382
SourceCodeDeleted <54761251+SourceCodeDeleted@users.noreply.github.com>
2025-11-29 18:53:21 +0400

    Update backend.tf

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "wiz-tfstate-backend"
-    key            = "wiz-exercise/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "wiz-tfstate-lock"
+    bucket  = "wiz-tfstate-backend"
+    key     = "wiz-exercise/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
