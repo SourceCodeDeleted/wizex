4f0b3dee73178b654619a36bab9719a12aef0ed7
SourceCodeDeleted <54761251+SourceCodeDeleted@users.noreply.github.com>
2025-11-29 18:44:06 +0400

    added backend

---
+++ deploy/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "wiz-tfstate-backend"
+    key            = "wiz-exercise/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "wiz-tfstate-lock"
+  }
+}
